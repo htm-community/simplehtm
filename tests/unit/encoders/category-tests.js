158ca1e7611082305d915a8c0540045163ca6afc
Matthew Taylor <matt@numenta.org>
2018-08-17 14:54:36 -0700

    Added some more category tests

--- tests/unit/encoders/category-tests.js
+++ tests/unit/encoders/category-tests.js
@@ -31,6 +31,34 @@ describe('category encoder', () => {
             expect(encoder.encode(false)).to.deep.equal([0,0,0,1,1,1])
         })
 
+        it('encodes days of week', () => {
+            let encoder = new CategoryEncoder({
+                w: 3,
+                categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+            })
+
+            expect(encoder.encode('Sun')).to.deep.equal([1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+            expect(encoder.encode('Mon')).to.deep.equal([0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+            expect(encoder.encode('Tue')).to.deep.equal([0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0])
+            expect(encoder.encode('Wed')).to.deep.equal([0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0])
+            expect(encoder.encode('Thu')).to.deep.equal([0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0])
+            expect(encoder.encode('Fri')).to.deep.equal([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0])
+            expect(encoder.encode('Sat')).to.deep.equal([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1])
+
+        })
+
+        it('encodes 3 flavors', () => {
+            let encoder = new CategoryEncoder({
+                w: 3,
+                categories: ['chocolate', 'vanilla', 'strawberry']
+            })
+
+            expect(encoder.encode('chocolate')).to.deep.equal([1,1,1,0,0,0,0,0,0])
+            expect(encoder.encode('vanilla')).to.deep.equal([0,0,0,1,1,1,0,0,0])
+            expect(encoder.encode('strawberry')).to.deep.equal([0,0,0,0,0,0,1,1,1])
+
+        })
+
     })
 
 })
