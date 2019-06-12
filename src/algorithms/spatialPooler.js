e2610d167081a73615552e0d29f9ebac954dda00
Matthew Taylor <matt@numenta.org>
2019-06-12 11:41:32 -0700

    Added SP overlap calculation

--- src/algorithms/spatialPooler.js
+++ src/algorithms/spatialPooler.js
@@ -1,47 +1,66 @@
 let d3 = require('d3')
 
-class SpatialPooler  {
+class SpatialPooler {
 
-	constructor(opts) {
-		this.opts = opts
-		this._potentialPools = this._createPotentialPools()
-		this._batesFn = d3.randomBates(this.opts.batesIndependentVariables || 10)
-		this._batesCenter = this.opts.distributionCenter || 0.5
-	}
+  constructor(opts) {
+    this.opts = opts
+    this._batesFn = d3.randomBates(this.opts.batesIndependentVariables || 10)
+    this._batesCenter = this.opts.distributionCenter || 0.5
+    this._potentialPools = this._createPotentialPools()
+    this._permanences = this._createPermanences()
+  }
 
-	getPotentialPools() {
-		return this._potentialPools
-	}
-  
-	_createPotentialPools() {
-		let pools = []
-		for (let i = 0; i < this.opts.size; i++) {
-			let pool = []
-			for (let j = 0; j < this.opts.inputCount; j++) {
-				if (Math.random() < this.opts.connectedPercent) {
-					pool.push(j)
-				}
-			}
-			pools.push(pool)
-		}
-		return pools
-	}
+  getPotentialPools() {
+    return this._potentialPools
+  }
 
-	getPermanences() {
-		const pools = this.getPotentialPools()
-		const allPerms = []
-		pools.forEach(pool => {
-			const perms = []
-			pool.forEach(_ => {
-				let perm = this._batesFn() + this._batesCenter - 0.5
-				if (perm > 1) perm = 1
-				if (perm < 0) perm = 0
-				perms.push(perm)
-			})
-			allPerms.push(perms)
-		})
-		return allPerms
-	}
+  getPermanences() {
+    return this._permanences
+  }
+
+  calculateOverlap(minicolumnIndex, input) {
+    const pool = this.getPotentialPools()[minicolumnIndex]
+    const perms = this.getPermanences()[minicolumnIndex]
+    let overlap = []
+    perms.forEach((perm, permIndex) => {
+      const inputIndex = pool[permIndex]
+      if (input[inputIndex] == 1 && perm >= this.opts.connectionThreshold) {
+        overlap.push(inputIndex)
+      }
+    })
+    return overlap
+  }
+
+  _createPotentialPools() {
+    let pools = []
+    for (let i = 0; i < this.opts.size; i++) {
+      let pool = []
+      for (let j = 0; j < this.opts.inputCount; j++) {
+        if (Math.random() < this.opts.connectedPercent) {
+          pool.push(j)
+        }
+      }
+      pools.push(pool)
+    }
+    return pools
+  }
+
+  _createPermanences() {
+    const pools = this.getPotentialPools()
+    const allPerms = []
+    pools.forEach(pool => {
+      const perms = []
+      pool.forEach(_ => {
+        let perm = this._batesFn() + this._batesCenter - 0.5
+        if (perm > 1) perm = 1
+        if (perm < 0) perm = 0
+        perms.push(perm)
+      })
+      allPerms.push(perms)
+    })
+    return allPerms
+  }
+
 }
 
 module.exports = SpatialPooler
--- tests/unit/algorithms/spatial-pooler-tests.js
+++ tests/unit/algorithms/spatial-pooler-tests.js
@@ -4,129 +4,200 @@ const d3 = require('d3')
 
 const SpatialPooler = require('../../../src/algorithms/spatialPooler')
 
-const connectedPercTestCases = [1, .75, .5, .25, 0.1]
 const defaultInputCount = 500
 const defaultSpSize = 200
 
 describe('upon SP instantiation', () => {
-	describe('when creating potential pools', () => {
-		
-		// Loop over a range of connected percents
-		connectedPercTestCases.forEach(connectedPercent => {
-		
-			describe(`with ${connectedPercent * 100}% connectivity`, () => {
 
-				// Lower connectionPercent means fewer connections to average across,
-				// so we need to add more tolerance for these.
-				const testTolerance = 0.05 + (1 - connectedPercent) * 0.1
+  const connectedPercTestCases = [1, .75, .5, .25, 0.1]
 
-				const inputCount = defaultInputCount
-				const spSize = defaultSpSize
-				const sp = new SpatialPooler({
-					// Assume 1D input, global inhibition, no topology
-					inputCount: inputCount,
-					// Assume 1D input, global inhibition, no topology
-					size: spSize,
-					connectedPercent: connectedPercent,
-				})
-		
-				it('contains potential pool indices', () => {
-					const pools = sp.getPotentialPools()
-					assert.lengthOf(pools, spSize, 
-						'pools should be same size as minicolumn count')
-					pools.forEach(p => {
-						p.forEach(poolIndex => {
-							// all indices are within range of input space
-							assert.isBelow(poolIndex, inputCount, 
-								'pool index out of input range')
-						})
-						assert.closeTo(p.length, inputCount * connectedPercent, inputCount * testTolerance,
-							`potential pool size should be approx ${connectedPercent} of the input space`)
-						assert.equal(new Set(p).size, p.length, 
-							'potential pool contains non-unique indices')
-					})
-				})
-			})
-		})
-	})
+  describe('when creating potential pools', () => {
 
-	describe('less than 100% connected potential pools are always the same', () => {
-		const inputCount = defaultInputCount
-		const spSize = defaultSpSize
-		const sp = new SpatialPooler({
-			// Assume 1D input, global inhibition, no topology
-			inputCount: inputCount,
-			// Assume 1D input, global inhibition, no topology
-			size: spSize,
-			connectedPercent: .85,
-		})
+    // Loop over a range of connected percents
+    connectedPercTestCases.forEach(connectedPercent => {
 
-		const pool1 = sp.getPotentialPools()
-		const pool2 = sp.getPotentialPools()
+      describe(`with ${connectedPercent * 100}% connectivity`, () => {
 
-		assert.deepEqual(pool1, pool2, 'potential pools should always be the same')
+        // Lower connectionPercent means fewer connections to average across,
+        // so we need to add more tolerance for these.
+        const testTolerance = 0.05 + (1 - connectedPercent) * 0.1
 
-	})
+        const inputCount = defaultInputCount
+        const spSize = defaultSpSize
+        const sp = new SpatialPooler({
+          // Assume 1D input, global inhibition, no topology
+          inputCount: inputCount,
+          // Assume 1D input, global inhibition, no topology
+          size: spSize,
+          connectedPercent: connectedPercent,
+        })
+
+        it('contains potential pool indices', () => {
+          const pools = sp.getPotentialPools()
+          assert.lengthOf(pools, spSize,
+            'pools should be same size as minicolumn count')
+          pools.forEach(p => {
+            p.forEach(poolIndex => {
+              // all indices are within range of input space
+              assert.isBelow(poolIndex, inputCount,
+                'pool index out of input range')
+            })
+            assert.closeTo(p.length, inputCount * connectedPercent, inputCount * testTolerance,
+              `potential pool size should be approx ${connectedPercent} of the input space`)
+            assert.equal(new Set(p).size, p.length,
+              'potential pool contains non-unique indices')
+          })
+        })
+      })
+    })
+  })
+
+  describe('less than 100% connected potential pools are always the same', () => {
+    const inputCount = defaultInputCount
+    const spSize = defaultSpSize
+    const sp = new SpatialPooler({
+      // Assume 1D input, global inhibition, no topology
+      inputCount: inputCount,
+      // Assume 1D input, global inhibition, no topology
+      size: spSize,
+      connectedPercent: .85,
+    })
+
+    const pool1 = sp.getPotentialPools()
+    const pool2 = sp.getPotentialPools()
+
+    assert.deepEqual(pool1, pool2, 'potential pools should always be the same')
+
+  })
+
+  describe('when establishing initial permanences', () => {
+
+    connectedPercTestCases.forEach(connectedPercent => {
+
+      describe(`with ${connectedPercent * 100}% connectivity`, () => {
+
+        [0.3, 0.5, 0.6].forEach(distributionCenter => {
+
+          describe(`around distribution center ${distributionCenter}`, () => {
+
+            const sp = new SpatialPooler({
+              // Assume 1D input, global inhibition, no topology
+              inputCount: defaultInputCount,
+              // Assume 1D input, global inhibition, no topology
+              size: defaultSpSize,
+              connectedPercent: connectedPercent,
+              distributionCenter: distributionCenter,
+            })
+            const allPerms = sp.getPermanences()
+            const pools = sp.getPotentialPools()
+
+            it('contains minicolumn permanences', function () {
+              this.timeout(0)
+              assert.lengthOf(allPerms, sp.opts.size,
+                'perms should be same size as minicolumn count')
+              allPerms.forEach((perms, minicolumnIndex) => {
+                const pool = pools[minicolumnIndex]
+                assert.lengthOf(perms, pool.length,
+                  `minicolumn ${minicolumnIndex} perms did not match its potential pool`)
+                perms.forEach((permanence, poolIndex) => {
+                  const inputIndex = pools[minicolumnIndex][poolIndex]
+                  assert.isAtMost(inputIndex, sp.opts.inputCount,
+                    'associated perm input index must be within input range')
+                  assert.isAtMost(permanence, 1.0,
+                    'permanence value cannot be larger than 1.0')
+                  assert.isAtLeast(permanence, 0.0,
+                    'permanence value cannot be smaller than 0.0')
+                })
+              })
+            })
+
+            it('permanences are normally distributed around specified center', () => {
+              // Lower connectionPercent means fewer connections to average across,
+              // so we need to add more tolerance for these.
+              const testTolerance = 0.05 + (1 - connectedPercent) * 0.1
+              allPerms.forEach((perms, minicolumnIndex) => {
+                assert.closeTo(d3.mean(perms), distributionCenter, testTolerance,
+                  `Average permanence across ${perms.length} permanences should be centered at ${distributionCenter}`)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+
+  describe('permanences are always the same between computations', () => {
+    const inputCount = defaultInputCount
+    const spSize = defaultSpSize
+    const sp = new SpatialPooler({
+      // Assume 1D input, global inhibition, no topology
+      inputCount: inputCount,
+      // Assume 1D input, global inhibition, no topology
+      size: spSize,
+      connectedPercent: .85,
+    })
+
+    const perms1 = sp.getPermanences()
+    const perms2 = sp.getPermanences()
+
+    assert.deepEqual(perms1, perms2, 'permanences should not change between computations')
+
+  })
 
 
-	describe('when establishing initial permanences', () => {
+})
 
-		connectedPercTestCases.forEach(connectedPercent => {
+describe('when calculating overlaps', () => {
 
-			describe(`with ${connectedPercent * 100}% connectivity`, () => {
+  const inputSaturations = [1, .75, .5, .25, 0.1]
 
-				[0.3, 0.5, 0.6].forEach(distributionCenter => {
+  const inputCount = 1000
+  const spSize = 500
+  const connectionThreshold = 0.5
+  const connectedPercent = 0.85
+  const sp = new SpatialPooler({
+    // Assume 1D input, global inhibition, no topology
+    inputCount: inputCount,
+    // Assume 1D input, global inhibition, no topology
+    size: spSize,
+    connectedPercent: connectedPercent,
+    connectionThreshold: connectionThreshold,
+  })
 
-					describe(`around distribution center ${distributionCenter}`, () => {
+  inputSaturations.forEach(inputSaturation => {
 
-						const sp = new SpatialPooler({
-							// Assume 1D input, global inhibition, no topology
-							inputCount: defaultInputCount,
-							// Assume 1D input, global inhibition, no topology
-							size: defaultSpSize,
-							connectedPercent: connectedPercent,
-							distributionCenter: distributionCenter,
-						})
-						const allPerms = sp.getPermanences()
-						const pools = sp.getPotentialPools()
+    describe(`when input is ${inputSaturation * 100}% saturated`, () => {
+      const input = []
+      for (let x = 0; x < inputCount; x++) {
+        input.push(Math.random() < inputSaturation ? 1 : 0)
+      }
+      it('overlap is within acceptable limits', () => {
+        for (let mcIndex = 0; mcIndex < spSize; mcIndex++) {
+          const overlapIndices = sp.calculateOverlap(mcIndex, input)
+          assert.isAtMost(Math.max(...overlapIndices), inputCount - 1,
+            'overlap index is above input range')
+          assert.isAtLeast(Math.min(...overlapIndices), 0,
+            'overlap index is below input range')
+          assert.equal(new Set(overlapIndices).size, overlapIndices.length,
+            'overlap contains non-unique indices')
+          const overlap = overlapIndices.length
+          const perms = sp.getPermanences()[mcIndex]
+          const totalConnections = perms.reduce((cons, perm) => {
+            let out = cons
+            if (perm >= connectionThreshold) {
+              out += 1
+            }
+            return out
+          }, 0)
+          assert.isAtMost(overlap, totalConnections,
+            'overlap should not be more than current connection count')
+          const observedOverlapPerc = overlap / totalConnections
+          assert.closeTo(observedOverlapPerc, inputSaturation, 0.1,
+            `observed overlap percent of total connections should be similar to input saturation`)
+        }
+      })
+    })
 
-						it('contains minicolumn permanences', function() {
-							this.timeout(0)
-							assert.lengthOf(allPerms, sp.opts.size, 
-								'perms should be same size as minicolumn count')
-							allPerms.forEach((perms, minicolumnIndex) => {
-								const pool = pools[minicolumnIndex]
-								assert.lengthOf(perms, pool.length, 
-									`minicolumn ${minicolumnIndex} perms did not match its potential pool`)
-								perms.forEach((permanence, poolIndex) => {
-									const inputIndex = pools[minicolumnIndex][poolIndex]
-									assert.isAtMost(inputIndex, sp.opts.inputCount, 
-										'associated perm input index must be within input range')
-									assert.isAtMost(permanence, 1.0, 
-										'permanence value cannot be larger than 1.0')
-									assert.isAtLeast(permanence, 0.0, 
-										'permanence value cannot be smaller than 0.0')
-								})
-							})
-						})
-			
-						it('permanences are normally distributed around specified center', () => {
-							// Lower connectionPercent means fewer connections to average across,
-							// so we need to add more tolerance for these.
-							const testTolerance = 0.05 + (1 - connectedPercent) * 0.1
-							allPerms.forEach((perms, minicolumnIndex) => {
-								assert.closeTo(d3.mean(perms), distributionCenter, testTolerance, 
-									`Average permanence across ${perms.length} permanences should be centered at ${distributionCenter}`)
-							})
-						})
-	
-					})
-
-				})
-
-			})
-		})
-
-	})
+  })
 
 })
